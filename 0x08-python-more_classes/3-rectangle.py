c47c4844a9a29724554166c1fd0bf1bae65c4abb
Eddith A <92625525+Dithsy@users.noreply.github.com>
2022-06-28 01:33:29 +0300

    three

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,8 +6,7 @@ class Rectangle:
     """Definition a class rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initializing Rectangle.
-        """
+        """Initializing Rectangle."""
         self.width = width
         self.height = height
 
@@ -52,4 +51,3 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return ((("#" * self.width) + "\n") * self.height)[:-1]
-
