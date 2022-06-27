61b2254d8c2d76de4540ea8aae2330c57be69201
Eddith A <92625525+Dithsy@users.noreply.github.com>
2022-06-28 02:07:16 +0300

    six

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-        Rectangle.number_of_instances = 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -20,9 +20,9 @@ class Rectangle:
     def width(self, value):
         self.__width = value
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -33,13 +33,13 @@ class Rectangle:
     def height(self, height):
         self.__height = height
         if not type(int):
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         if height < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError('height must be >=0')
         self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
@@ -63,3 +63,4 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+ 
